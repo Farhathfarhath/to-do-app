dd4513294ac6e985891d6265b2f9cecc9d6ef4ff
Farhath <farhath0535@gmail.com>
2026-02-01 16:27:48 +0530

    Update script.js

---
+++ script.js
@@ -0,0 +1,26 @@
+body {
+  font-family: Arial, sans-serif;
+  background: #f4f4f4;
+}
+
+.container {
+  width: 300px;
+  margin: 100px auto;
+  background: white;
+  padding: 20px;
+  border-radius: 5px;
+}
+
+input {
+  width: 70%;
+  padding: 5px;
+}
+
+button {
+  padding: 6px 10px;
+  cursor: pointer;
+}
+
+li {
+  margin-top: 10px;
+}
